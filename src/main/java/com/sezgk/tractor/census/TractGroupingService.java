d1ff6205f87077783ba70d114bf785014fc33c6c
thegarythompson <Gary6@umbc.edu>
2014-10-21 00:31:34 -0400

    TractGroupingService now uses the Quicksort to generate the list of districts.

--- src/main/java/com/sezgk/tractor/census/TractGroupingService.java
+++ src/main/java/com/sezgk/tractor/census/TractGroupingService.java
@@ -4,6 +4,8 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.sezgk.tractor.util.Quicksort;
+
 
 
 public class TractGroupingService {
@@ -11,11 +13,20 @@ public class TractGroupingService {
 
 	public static List<CongressionalDistrict> createDistricts(List<CensusTract> tracts) {
 		
+		
+		//Define the starting NW corner
+		BigDecimal testLat = new BigDecimal(39.721077);
+		BigDecimal testLong = new BigDecimal(-79.476661);
+		MapCoordinate testCoord = new MapCoordinate(testLat, testLong);
+		
 		List<CongressionalDistrict> districts = new ArrayList<CongressionalDistrict>();
+		Quicksort sorter = new Quicksort(tracts, testCoord);
+		tracts=sorter.getSortedCensusList();
 		
+		//Maryland-specific number of districts
 		int numDistricts = 8;
-		int statePopulation = 0;
 		
+		int statePopulation = 0;
 		int districtPop = 0;
 		int districtCounter=0;
 		
@@ -24,8 +35,17 @@ public class TractGroupingService {
 			statePopulation+=tracts.get(i).getPopulation();
 			
 		}
+		
+		System.out.println(statePopulation);
+		
+		//Target population for each district
 		int targetPop = statePopulation/numDistricts;
 				
+		//Loop for each district: 
+		//- add a district
+		//- add tracts until the target population is reached. As each tract is added to a district,
+		//		it is removed from the list of tracts
+		//- sort the list by distance from the first tract in the new district
 		for (districtCounter=0; districtCounter<numDistricts; districtCounter++)
 		{
 			districts.add(new CongressionalDistrict());
@@ -35,21 +55,26 @@ public class TractGroupingService {
 				tracts.remove(0);
 				
 			}
-			//sortDistance(tracts);
+			tracts=sorter.getSortedCensusList();
 			districtPop=0;
 		}
 		
+		//Catch any missed tracts and add to the last district
+		System.out.println(tracts.size());
+		while (tracts.size()>0)
+		{
+			districtPop+=districts.get(numDistricts-1).addTract(tracts.get(0));
+			tracts.remove(0);
+		}
+		
+		int sum=0;
+		for (int i=0; i<districts.size(); i++)
+		{
+			sum+=districts.get(i).getDistrictPop();
+		}
+		System.out.println(sum);
 		return districts;
 		
 	}
-
-	private static void sortDistance(List<CensusTract> tracts) {
-		
-	}
-	
-	
-	
-	
-	
 
 }
