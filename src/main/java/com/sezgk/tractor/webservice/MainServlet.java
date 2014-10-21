1d31bdbf603868a09ea89b6768c0a6cf2fa4c86c
iwantmybird <e.golaszewski@gmail.com>
2014-10-21 12:58:02 -0400

    Updated site and servlet to leverage districts.
    You can now see districts be drawn on the map. It would seem our
    algorithm needs a bit of work, but it is doing something!

--- src/main/java/com/sezgk/tractor/webservice/MainServlet.java
+++ src/main/java/com/sezgk/tractor/webservice/MainServlet.java
@@ -2,10 +2,12 @@ package com.sezgk.tractor.webservice;
 
 import java.io.IOException;
 import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.ContextHandler;
@@ -13,9 +15,12 @@ import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.sezgk.tractor.census.CensusTract;
+import com.sezgk.tractor.census.CongressionalDistrict;
+import com.sezgk.tractor.census.TractGroupingService;
 import com.sezgk.tractor.census.parser.ParsingService;
 
 /**
@@ -33,8 +38,11 @@ public class MainServlet extends HttpServlet
         ParsingService p = new ParsingService();
         List<CensusTract> tracts = p.parseTracts();
         
+        // TODO change this static call.
+        List<CongressionalDistrict> districts = TractGroupingService.createDistricts(tracts);
+        
         Gson gson = new GsonBuilder().create();
-        String json = gson.toJson(tracts);
+        String json = gson.toJson(districts);
         
         resp.setContentType("text/json");
         resp.getWriter().write(json);
